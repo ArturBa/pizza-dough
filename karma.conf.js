ea0198188f6116b3cdd26c0129fa922121d5367e
Artur Bauer <bauerart1@gmail.com>
2022-12-12 13:48:19 +0100

    ci: :construction_worker: add a merge request CI

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       jasmine: {
@@ -16,10 +16,10 @@ module.exports = function (config) {
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
       },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/'),
@@ -28,8 +28,8 @@ module.exports = function (config) {
         { type: 'html' },
         { type: 'text-summary' },
         { type: 'cobertura' },
-        { type: 'lcov' }
-      ]
+        { type: 'lcov' },
+      ],
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
@@ -38,7 +38,6 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
   });
 };
-
