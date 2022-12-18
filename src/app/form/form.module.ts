0d6d9f3f9a3187d96a34e9e74c517d6ab9e440ea
Artur Bauer <bauerart1@gmail.com>
2022-12-18 13:29:17 +0100

    fix: failing test

--- src/app/form/form.module.ts
+++ src/app/form/form.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipeSelectorComponent } from './recipe-selector/recipe-selector.component';
 import { OvenSelectorComponent } from './oven-selector/oven-selector.component';
+import { ControlModule } from '../components/control/control.module';
 
 @NgModule({
   declarations: [RecipeSelectorComponent, OvenSelectorComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ControlModule],
 })
 export class FormModule {}
--- src/app/form/recipe-selector/recipe-selector.component.spec.ts
+++ src/app/form/recipe-selector/recipe-selector.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RecipeSelectorComponent } from './recipe-selector.component';
+import { ControlModule } from '../../components/control/control.module';
 
 describe('RecipeSelectorComponent', () => {
   let component: RecipeSelectorComponent;
@@ -9,6 +10,7 @@ describe('RecipeSelectorComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RecipeSelectorComponent],
+      imports: [ControlModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RecipeSelectorComponent);
