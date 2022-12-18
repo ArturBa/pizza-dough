36ebd10d135341dd46e6d700cfb0feb5214fc33e
Artur Bauer <bauerart1@gmail.com>
2022-12-18 14:13:26 +0100

    feat(form): :sparkles: prepare form input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { AnalyticsComponent } from './analytics/analytics/analytics.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
+import { FormModule } from './form/form.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AnalyticsModule, ComponentsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AnalyticsModule,
+    ComponentsModule,
+    FormModule,
+  ],
   providers: [],
   bootstrap: [AppComponent, AnalyticsComponent],
 })
