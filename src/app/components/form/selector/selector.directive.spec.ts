f003c2c967720b9ce55eb512e2d40f6c9607b943
Artur Bauer <bauerart1@gmail.com>
2022-12-18 12:04:58 +0100

    test: update tests for selector

--- src/app/components/form/selector/selector.directive.spec.ts
+++ src/app/components/form/selector/selector.directive.spec.ts
@@ -1,8 +1,81 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SelectorDirective } from './selector.directive';
+import { FormFieldComponent } from '../form-field/form-field.component';
+import {
+  ElementFromFixture,
+  getElementFromFixture,
+} from '../form-field/form-field.component.spec';
 
+@Component({
+  selector: 'app-test-selector',
+  template: `
+    <app-form-field>
+      <select appSelector>
+        <option *ngFor="let value of values" [value]="value">
+          {{ value }}
+        </option>
+      </select>
+    </app-form-field>
+  `,
+})
+class TestSelectorComponent {
+  values = ['One'];
+}
 describe('SelectorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SelectorDirective();
-    expect(directive).toBeTruthy();
+  let component: TestSelectorComponent;
+  let fixture: ComponentFixture<TestSelectorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        TestSelectorComponent,
+        SelectorDirective,
+        FormFieldComponent,
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestSelectorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe('select component', () => {
+    let compiled: ElementFromFixture<HTMLSelectElement>;
+
+    beforeEach(async () => {
+      fixture.detectChanges();
+      compiled = getElementFromFixture(fixture, 'select');
+    });
+
+    it('should create the selector', () => {
+      expect(compiled).toBeTruthy();
+    });
+
+    it('should have a selector directive class', () => {
+      const classes = compiled?.classList;
+      expect(classes).toContain('app-select');
+    });
+
+    it('should have a input directive class', () => {
+      const classes = compiled?.classList;
+      expect(classes).toContain('app-input');
+    });
+  });
+
+  describe('chevron component', () => {
+    let compiled: ElementFromFixture;
+
+    beforeEach(async () => {
+      compiled = getElementFromFixture(fixture, 'app-chevron');
+    });
+
+    it('should create the selector', () => {
+      expect(compiled).toBeTruthy();
+    });
   });
 });
