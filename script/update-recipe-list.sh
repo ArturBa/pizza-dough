10839f3947a5edebc4d4858042a298608f95f23c
Artur Bauer <bauerart1@gmail.com>
2022-12-18 17:17:02 +0100

    chore: add recipes  (#19)
    * chore: add recipes
    
    * chore: update git-ci
    
    * chore: update git-ci
    
    * chore: update git-ci
    
    * chore: update git-ci

---
+++ script/update-recipe-list.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+RECIPE_LIST_FILE="src/assets/data/list.json"
+
+get_recipe_title() {
+  grep -ohP '(?<="title": ").*(?=")' "$1"
+}
+
+get_recipe_oven() {
+  grep -ohP '(?<="oven": ").*(?=")' "$1"
+}
+
+
+echo "[" > $RECIPE_LIST_FILE
+for file in src/assets/data/recipes/*.json
+do
+    id=$(echo "$file" | grep -oe '[0-9]*')
+    (
+      printf '{\n'
+      printf '\t"id": "%s",' "$id"
+      printf '\t"title": "%s",' "$(get_recipe_title "$file")"
+      printf '\t"oven": "%s"' "$(get_recipe_oven "$file")"
+      printf '},'
+    ) >> $RECIPE_LIST_FILE
+done
+
+echo "]" >> $RECIPE_LIST_FILE
