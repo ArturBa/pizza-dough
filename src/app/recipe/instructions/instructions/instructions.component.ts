6626545e99739dd77317855a7bb848aebb51c0bd
Artur Bauer <bauerart1@gmail.com>
2023-01-16 13:43:35 +0100

    feat(recipe): :sparkles: display recipe details

--- src/app/recipe/instructions/instructions/instructions.component.ts
+++ src/app/recipe/instructions/instructions/instructions.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { RecipeState } from '../../redux/recipe.store';
+import { Observable, filter, map } from 'rxjs';
+import { RecipeItem } from '../../redux/recipe.model';
 
 @Component({
   selector: 'app-instructions',
   templateUrl: './instructions.component.html',
   styleUrls: ['./instructions.component.scss'],
 })
-export class InstructionsComponent {}
+export class InstructionsComponent {
+  @Select(RecipeState.getSelectedRecipe)
+  protected readonly recipe$!: Observable<RecipeItem | null>;
+
+  get recipeDetails$(): Observable<string | undefined> {
+    return this.recipe$.pipe(
+      filter(recipe => !!recipe),
+      map(recipe => recipe?.recipe)
+    );
+  }
+}
