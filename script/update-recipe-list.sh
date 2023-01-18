bb7a00decb48ea48d164592dc3bc574b00fd2ed7
Artur Bauer <bauerart1@gmail.com>
2023-01-18 13:23:39 +0100

    feat: add style into common data

--- script/update-recipe-list.sh
+++ script/update-recipe-list.sh
@@ -10,6 +10,10 @@ get_recipe_oven() {
   grep -ohP '(?<="oven": ").*(?=")' "$1"
 }
 
+get_recipe_style() {
+  grep -ohP '(?<="style": ").*(?=")' "$1"
+}
+
 
 echo "[" > $RECIPE_LIST_FILE
 for file in src/assets/data/recipes/*.json
@@ -19,7 +23,8 @@ do
       printf '{\n'
       printf '\t"id": "%s",' "$id"
       printf '\t"title": "%s",' "$(get_recipe_title "$file")"
-      printf '\t"oven": "%s"' "$(get_recipe_oven "$file")"
+      printf '\t"oven": "%s",' "$(get_recipe_oven "$file")"
+      printf '\t"style": "%s"' "$(get_recipe_style "$file")"
       printf '},'
     ) >> $RECIPE_LIST_FILE
 done
