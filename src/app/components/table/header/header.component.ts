d6222322f30078eb7e3fedfb3ef822a11b5da2c2
Artur Bauer <bauerart1@gmail.com>
2023-01-26 14:17:38 +0100

    feat(component): :sparkles: add a mobile view for table (#34)

--- src/app/components/table/header/header.component.ts
+++ src/app/components/table/header/header.component.ts
@@ -1,4 +1,26 @@
 import { Component, Input } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { RowDataInterface } from '../row/row.component';
+
+export interface TableHeaderInterface {
+  key: keyof RowDataInterface;
+  label: string;
+}
+
+const newMapHeader = (unit: string): Map<keyof RowDataInterface, string> =>
+  new Map([
+    ['title', ''],
+    ['bakers', "Bakers' %"],
+    ['weight', `Weight (${unit})`],
+    ['recommended', 'Recommended'],
+  ]);
+
+const updateTableHeaders = (unit: string) => {
+  tableHeaders$.next(newMapHeader(unit));
+};
+
+export const tableHeaders$: ReplaySubject<Map<keyof RowDataInterface, string>> =
+  new ReplaySubject();
 
 @Component({
   selector: 'app-header',
@@ -6,5 +28,22 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  @Input() unit!: string;
+  protected _unit = '';
+  tableHeaderOrder: (keyof RowDataInterface)[] = [
+    'title',
+    'bakers',
+    'weight',
+    'recommended',
+  ];
+
+  @Input() set unit(value: string) {
+    this._unit = value;
+    updateTableHeaders(value);
+  }
+
+  tableHeaders$ = tableHeaders$;
+
+  constructor() {
+    tableHeaders$.subscribe(console.log);
+  }
 }
